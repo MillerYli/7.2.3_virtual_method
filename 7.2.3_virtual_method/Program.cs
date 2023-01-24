f09c58d6a326eebca066b3186bee440525d2bd52
Yulia Miller <miller82.98@mail.ru>
2023-01-24 12:06:13 -0800

    Добавьте файлы проекта.

---
+++ 7.2.3_virtual_method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+BaseClass bas = new BaseClass();
+bas.Display();
+
+DerivedClass derived= new DerivedClass();
+derived.Display();
+class BaseClass
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("Метод класса BaseClass");
+    }
+}
+class DerivedClass : BaseClass
+{
+    public override void Display()
+    {
+        Console.WriteLine("Метод класса DeriverClass");
+    }
+}
